428c92c81a7b6148983581b30447230645cae6f5
lakhalsahar <lakhalsahar@isimsf.u-sfax.tn>
2026-01-16 16:24:02 +0100

    updated frontend

---
+++ src/app/Todo.ts
@@ -0,0 +1,32 @@
+export type Status = "TODO" | "IN_PROGRESS" | "DONE" | "MISSED";
+export type Priority = "LOW" | "MEDIUM" | "HIGH";
+
+export interface Todo {
+  id: number;
+  title: string;
+  description?: string;
+  status: Status;
+  priority: Priority;
+  dueDate?: string;
+}
+
+export interface CreateRequest {
+  title: string;
+  description?: string;
+  priority?: Priority;
+  dueDate?: string;
+}
+
+export interface UpdateRequest {
+  title: string;
+  description?: string;
+  status?: Status;
+  priority?: Priority;
+  dueDate?: string;
+}
+
+export interface ProgressResponse {
+  total: number;
+  done: number;
+  percentage: number;
+}
